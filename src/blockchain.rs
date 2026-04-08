eab3a34ccebae7d9f2665548d93b4d3ccf75dac4
MKNSTEJA <chessmaniacs123@gmail.com>
2026-04-08 23:49:03 +0200

    Added add_block functionality to blockchain to add blocks

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,5 +1,5 @@
 use crate::block::Block;
-// use crate::transaction::Transaction;
+use crate::transaction::Transaction;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
@@ -15,7 +15,7 @@ impl Blockchain{
         };
 
         blockchain.create_genesis_block();
-        return blockchain;
+        blockchain
     }
 
     fn create_genesis_block(&mut self){
@@ -25,5 +25,16 @@ impl Blockchain{
             String::from("0000000000000000"),
         );
         self.chain.push(genesis_block);
+    }
+
+    pub fn add_block(&mut self, transactions: Vec<Transaction>){
+        let prev_block = self.chain.last().unwrap();
+
+        let new_index = prev_block.index + 1;
+        let prev_hash = prev_block.hash.clone();
+
+        let new_block = Block::new(new_index, transactions, prev_hash);
+
+        self.chain.push(new_block);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,19 @@ use block::Block;
 use blockchain::Blockchain;
 
 fn main() {
-    let first_tx = Transaction::new(
-        String::from("Alice"),
-        String::from("Bob"),
-        100.50,
-    );
 
-    let tx_list = vec![first_tx.clone()];
-    let blockchain = Blockchain::new();
-    let first_block = Block::new(
-        1,
-        tx_list,
-        String::from("0000000000000000"),
-    );
+    let mut blockchain = Blockchain::new();
 
+    println!("Mining Block 1");
+    let tx1 = Transaction::new(String::from("Alice"), String::from("Bob"), 100.50);
+    let tx2 = Transaction::new(String::from("Alice"), String::from("Charlie"), 300.20);
 
+    let transactions = vec![tx1, tx2];
+    blockchain.add_block(transactions);
 
-    println!("Successfully created {:?}", &first_tx);
+    println!("Mining Block 2");
+    let tx3 = Transaction::new(String::from("Dominic"), String::from("Denis"), 1350.2);
+    blockchain.add_block(vec![tx3]);
+
+    println!("{:#?}", blockchain);
 }
